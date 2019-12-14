33b4bc295bc0da5e6ed16af295cc4353d6325ab9
kievtechnoshop@gmail.com <swr83947>
2019-12-14 16:01:13 +0200

    Updates

---
+++ precache-manifest.4dcd79fd657e11c68f8ed07b91be1370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb668cd2fbf7d37860634b3a748e598",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "44720688b28ca6ddc3fb",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "165d1c47e95fb93fe81c",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.9d813835.chunk.js"
+  },
+  {
+    "revision": "44720688b28ca6ddc3fb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.2bef0298.chunk.js"
+  },
+  {
+    "revision": "01e99cb9f9299fb1cbd1",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.bece1991.js"
+  }
+]);
