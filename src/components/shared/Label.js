7c3587f3c8466d17c59ffe36116be4b5f3aaff3b
kievtechnoshop@gmail.com <swr83947>
2019-12-14 15:57:17 +0200

    5

---
+++ src/components/shared/Label.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledLabel = styled.label`
+  display: inline-block;
+  font-size: 20px;
+  color: #171718;
+  cursor: pointer;
+  ${props => props.customStyles}
+`;
+
+const Label = ({ children, customStyles }) => (
+  <StyledLabel customStyles={customStyles}>{children}</StyledLabel>
+);
+
+export default Label;
